894e004e525c90938e42b4f78cf2924e8ab88a03
Krisna Sirait <krisnapande00@outlook.com>
2022-06-29 13:18:52 +0700

    initial commit

---
+++ src/Box.kt
@@ -0,0 +1,16 @@
+class Box { //class declaration
+    //properties (val immutable, var mutable)
+    val length: Int = 10
+    val width: Int = 20
+    val height: Int = 5
+
+    //behaviors 1
+    fun fillContents(){
+        println("Box is filled")
+    }
+
+    //behaviors 2
+    fun openBox(){
+        println("Box Opened")
+    }
+}
